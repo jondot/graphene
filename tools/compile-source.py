41f3d880005683db72493dc1d7b250d87b4c2e28
Boopathi Rajaa <me@boopathi.in>
2013-01-29 14:59:50 +0530

    Directly access the coffeescript file rather than a manual compilation step

--- tools/compile-source.py
+++ tools/compile-source.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python
 
 import httplib, urllib, sys
+import subprocess
 
-with open('../app/js/graphene.js') as graphene_file:
-    graphene_content = graphene_file.read()
+#with open('../app/js/graphene.js') as graphene_file:
+#    graphene_content = graphene_file.read()
+
+p = subprocess.Popen(['/usr/bin/coffee', '-cp', '../app/js/graphene.coffee'],stdout=subprocess.PIPE)
+graphene_data = p.communicate()
 
 params = urllib.urlencode([
     ('code_url', 'http://code.jquery.com/jquery-1.8.0.min.js'), # jquery
     ('code_url', 'http://underscorejs.org/underscore-min.js'), # underscore
     ('code_url', 'http://backbonejs.org/backbone-min.js'), # backbone
     ('code_url', 'http://d3js.org/d3.v3.min.js'), # d3
-    ('js_code', graphene_content),
+    ('js_code', graphene_data),
     ('compilation_level', 'SIMPLE_OPTIMIZATIONS'),
     ('output_format','text'),
     ('output_info','compiled_code'),
